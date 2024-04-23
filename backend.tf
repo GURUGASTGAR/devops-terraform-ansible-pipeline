224d5032abc6ed5f1e17a3821750a6a92d830827
Guruprasad <100029720+GURUGASTGAR@users.noreply.github.com>
2024-04-24 02:32:17 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket = "tf-buket-batch759-guru"
+    bucket = "tf-buket-batch759-guru-east"
     key    = "project/remote.tfstate"
     region = "us-east-1"
   }
